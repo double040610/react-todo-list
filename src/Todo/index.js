e0bae66f2208eda26b4c89130963240bb48cc346
Your Name <you@example.com>
2022-01-04 00:00:40 +0800

    完成单选多选删除

--- src/Todo/index.js
+++ src/Todo/index.js
@@ -1,14 +1,31 @@
+import { useStore } from '../store'
+import { observer } from 'mobx-react-lite'
 import './index.css'
+
 function Cart() {
+  const { taskStore } = useStore()
+  // 单选
+  const singleCheck = (id, e) => {
+    taskStore.singleCheck(id, e.target.checked)
+  }
+  // 删除
+  const delCart = (id) => {
+    taskStore.delCart(id)
+  }
+  // 全选
+  const allCheck = (e) => {
+    taskStore.allCheck(e.target.checked)
+  }
   return (
     <section className="todoapp">
       <header className="header">
         <h1>todos</h1>
         <input
           className="new-todo"
-          autofocus
-          autocomplete="off"
+          autoFocus
+          autoComplete="off"
           placeholder="What needs to be done?"
+
         />
       </header>
       <section className="main">
@@ -16,22 +33,33 @@ function Cart() {
           id="toggle-all"
           className="toggle-all"
           type="checkbox"
+          onChange={allCheck}
+          checked={taskStore.isAll}
         />
-        <label for="toggle-all"></label>
+        <label htmlFor="toggle-all"></label>
         <ul className="todo-list">
-          <li
-            className="todo"
-          >
-            <div className="view">
-              <input className="toggle" type="checkbox" />
-              <label >learn react</label>
-              <button className="destroy"></button>
-            </div>
-          </li>
+          {
+            taskStore.list.map(task => (
+              <li
+                key={task.id}
+                className={task.done ? 'completed todo' : 'todo'}
+              >
+                <div className="view">
+                  <input
+                    className="toggle"
+                    type="checkbox"
+                    checked={task.done}
+                    onChange={(e) => singleCheck(task.id, e)} />
+                  <label > {task.title} </label>
+                  <button className="destroy" onClick={() => delCart(task.id)}></button>
+                </div>
+              </li>
+            ))
+          }
         </ul>
       </section>
     </section>
   )
 }
 
-export default Cart
+export default observer(Cart)
--- src/store/cart.Store.js
+++ src/store/cart.Store.js
@@ -1,20 +1,36 @@
 
-import {  makeAutoObservable } from 'mobx'
+import { makeAutoObservable } from 'mobx'
 class TaskStore {
   list = [
     {
-      id:1,
-      name: '学习react',
-      isDone: true
+      id: 1,
+      title: '学习react',
+      done: true
     },
     {
-      id:2,
-      name: '搞定mobx',
-      isDone: true
+      id: 2,
+      title: '搞定mobx',
+      done: true
     }
   ]
   constructor() {
     makeAutoObservable(this)
+  }
+  get isAll() {
+    return this.list.every(item => item.done)
+  }
+  // 单选
+  singleCheck(id, checked) {
+    const target = this.list.find(item => item.id === id)
+    target.done = checked
+  }
+  // 删除
+  delCart(id) {
+    this.list = this.list.filter(item => item.id !== id)
+  }
+  // 全选
+  allCheck(checked) {
+    this.list.forEach(item => item.done = checked)
   }
 }
 export default TaskStore
